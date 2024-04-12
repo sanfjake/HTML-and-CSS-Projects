d959cfcdcfe7cfee99c586af7d6aa6684dd932c2
sanfjake <jake.w.sanford@gmail.com>
2024-04-12 15:59:09 -0600

    Basic_HTML_@

--- Basic_HTML_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,11 @@
 alert("Hello, World! I am an alert box!")
+
+<script>
+        function My_First_Function() {
+            var String = "Kiss me, I'm Irish!";
+            var reslut = String.fontcolor("green");
+            document.getElementByID("Irish") .innerHTML = 
+            result;
+        }
+  </script>
+  
